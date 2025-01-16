c7d914da3b5b58a5f54c1b69eabb77c2b39d332b
Daniel Schloms <dschloms@dsgpu.ecs.tuwien.ac.at>
2025-01-16 18:07:02 +0100

    Add second container with Ubuntu 22.04

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-docker build -t etiss-20_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+if [ $1 = "20" ]; then
+  echo "Building Ubuntu 20.04 version"
+  cd ubuntu20_04
+  docker build -t etiss-20_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+  exit 0
+fi
+
+if [ $1 = "22" ]; then
+  echo "Building Ubuntu 22.04 version"
+  cd ubuntu22_04
+  docker build -t etiss-22_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+  exit 0
+fi
+
+echo "Specify Ubuntu version (20 or 22)"
