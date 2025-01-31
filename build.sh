a5cee5408fe844ca5acbf58d2f0800d42ef22d5a
Daniel Schloms <daniel.schloms@gmail.com>
2025-01-31 17:38:19 +0100

    WIP: Add more distinctions

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-if [ $1 = "20" ]; then
+if [ $1 = "etiss20" ]; then
   echo "Building Ubuntu 20.04 version"
   cd ubuntu20_04
   docker build -t etiss-20_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
   exit 0
 fi
 
-if [ $1 = "22" ]; then
-  echo "Building Ubuntu 22.04 version"
-  cd ubuntu22_04
-  docker build -t etiss-22_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+if [ $1 = "etiss24" ]; then
+  echo "Building Ubuntu 24.04 version for ETISS"
+  cd ubuntu24_04
+  docker build -t etiss-24_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+  exit 0
+fi
+
+if [ $1 = "ml22" ]; then
+  echo "Building Ubuntu 22.04 version for ML-ON-MCU"
+  cd mlonmcu
+  docker build -t mlonmcu-22_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
   exit 0
 fi
 
