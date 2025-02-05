0f975ebf23f6d4fc3a4cd5dd8632f6008bc34a09
Daniel Schloms <daniel.schloms@gmail.com>
2025-02-05 15:22:40 +0100

    Update Dockerfiles and build script

--- build.sh
+++ build.sh
@@ -15,10 +15,21 @@ if [ $1 = "etiss24" ]; then
 fi
 
 if [ $1 = "ml22" ]; then
-  echo "Building Ubuntu 22.04 version for ML-ON-MCU"
+  echo "Building Ubuntu 22.04 version for ML on MCU"
   cd mlonmcu
   docker build -t mlonmcu-22_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
   exit 0
 fi
 
-echo "Specify Ubuntu version (20 or 22)"
+if [ $1 = "perfsim24" ]; then
+  echo "Building Ubuntu 24.04 version for performance estimator"
+  cd perfsim
+  docker build -t perfsim-24_04 --build-arg USERNAME=$USER --build-arg USER_UID=$UID --build-arg USER_GID=$(id -g $USER) .
+  exit 0
+fi
+
+echo "Specify build"
+echo "  - etiss20"
+echo "  - etiss24"
+echo "  - ml22"
+echo "  - perfsim24"
